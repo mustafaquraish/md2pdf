1e353eca2cc32769daa0a526e639782466f27d7e
Mustafa Quraish <mustafa.quraish@mail.utoronto.ca>
2020-06-12 06:49:14 -0400

    testing

--- api/index.py
+++ api/index.py
@@ -2,7 +2,10 @@ from flask import Flask, Response
 app = Flask(__name__)
 
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def catch_all(path):
-    return Response("<h1>Flask</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
+@app.route('/')
+def catch_all():
+    return Response("HELLOOOOOOOO")
+
+@app.route('/pls')
+def catch_all():
+    return Response("HELLOOOOOOOO")
